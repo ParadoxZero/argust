4e93ab32c1e174ece46a7bb059f0be5f6a6d0727
Sidhin S Thomas <thomas.sidhin@outlook.com>
2023-02-11 00:58:25 +0530

    Make tests cleaner

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,6 @@
 use argust::*;
 
-use std::{ffi::OsString, str::FromStr};
+use std::ffi::OsString;
 
 fn compare(lhs: &Vec<String>, rhs: &Vec<String>) -> bool {
     println!("{:?} || {:?}", lhs, rhs);
@@ -27,16 +27,10 @@ fn basic_test() {
         "Command2",
     ];
     let command_set = parse_args(args.iter(), None);
-    let expected_output = vec![
-        String::from_str("Command1").unwrap(),
-        String::from_str("Command2").unwrap(),
-    ];
+    let expected_output = vec![String::from("Command1"), String::from("Command2")];
     assert!(compare(&command_set.args, &expected_output));
 
-    let expected_output = vec![
-        String::from_str("h").unwrap(),
-        String::from_str("q").unwrap(),
-    ];
+    let expected_output = vec![String::from("h"), String::from("q")];
     assert!(compare(&command_set.switches, &expected_output));
     assert_eq!(command_set.options["Option1"], "");
     assert_eq!(command_set.options["Option2"], "test");
@@ -45,25 +39,19 @@ fn basic_test() {
 #[test]
 fn with_string_test() {
     let args: Vec<String> = vec![
-        String::from_str("Command1").unwrap(),
-        String::from_str("-h").unwrap(),
-        String::from_str("-q").unwrap(),
-        String::from_str("--Option1").unwrap(),
-        String::from_str("--Option2=test").unwrap(),
-        String::from_str("Command2").unwrap(),
+        String::from("Command1"),
+        String::from("-h"),
+        String::from("-q"),
+        String::from("--Option1"),
+        String::from("--Option2=test"),
+        String::from("Command2"),
     ];
     let args = args.iter();
     let command_set = parse_args(args, None);
-    let expected_output = vec![
-        String::from_str("Command1").unwrap(),
-        String::from_str("Command2").unwrap(),
-    ];
+    let expected_output = vec![String::from("Command1"), String::from("Command2")];
     assert!(compare(&command_set.args, &expected_output));
 
-    let expected_output = vec![
-        String::from_str("h").unwrap(),
-        String::from_str("q").unwrap(),
-    ];
+    let expected_output = vec![String::from("h"), String::from("q")];
     assert!(compare(&command_set.switches, &expected_output));
     assert_eq!(command_set.options["Option1"], "");
     assert_eq!(command_set.options["Option2"], "test");
@@ -72,24 +60,18 @@ fn with_string_test() {
 #[test]
 fn with_os_string_test() {
     let args: Vec<OsString> = vec![
-        OsString::from_str("Command1").unwrap(),
-        OsString::from_str("-h").unwrap(),
-        OsString::from_str("-q").unwrap(),
-        OsString::from_str("--Option1").unwrap(),
-        OsString::from_str("--Option2=test").unwrap(),
-        OsString::from_str("Command2").unwrap(),
+        OsString::from("Command1"),
+        OsString::from("-h"),
+        OsString::from("-q"),
+        OsString::from("--Option1"),
+        OsString::from("--Option2=test"),
+        OsString::from("Command2"),
     ];
     let command_set = parse_args(args.iter(), None);
-    let expected_output = vec![
-        String::from_str("Command1").unwrap(),
-        String::from_str("Command2").unwrap(),
-    ];
+    let expected_output = vec![String::from("Command1"), String::from("Command2")];
     assert!(compare(&command_set.args, &expected_output));
 
-    let expected_output = vec![
-        String::from_str("h").unwrap(),
-        String::from_str("q").unwrap(),
-    ];
+    let expected_output = vec![String::from("h"), String::from("q")];
     assert!(compare(&command_set.switches, &expected_output));
     assert_eq!(command_set.options["Option1"], "");
     assert_eq!(command_set.options["Option2"], "test");
